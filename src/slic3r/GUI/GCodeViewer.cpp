fc07504432d8dce3f91dc91736aee6ea9ba95ccb
bickus <bickus@live.com>
2025-12-31 23:41:56 +0200

    Now nicer

--- src/slic3r/GUI/GCodeViewer.cpp
+++ src/slic3r/GUI/GCodeViewer.cpp
@@ -502,31 +502,45 @@ void GCodeViewer::SequentialView::Marker::render(int canvas_width, int canvas_he
             break;
         }
         case EViewType::Feedrate: {
-            ImGui::SameLine(startx2);
+            // Center the "Requested Speed" line (empty line already added by else branch above)
             sprintf(buf, "%s%.0f", speed.c_str(), m_curr_move.feedrate);
-            ImGui::PushItemWidth(item_size);
+            float text_width = ImGui::CalcTextSize(buf).x;
+            float window_width = ImGui::GetWindowWidth();
+            ImGui::SetCursorPosX((window_width - text_width) * 0.5f);
             imgui.text(buf);
+
             // Speed modifier tracking: show modifier chain if available
             if (m_curr_move.has_speed_modifiers()) {
-                ImGui::NewLine();
-                // Show base speed with optional type label
+                // Show base speed - use type label directly for special types (Bridge, Gap Fill, etc.)
                 const char* base_label = get_base_type_label(m_curr_move.base_type);
-                if (base_label[0] != '\0')
-                    sprintf(buf, "Base: %.0f (%s)", m_curr_move.base_speed, base_label);
-                else
-                    sprintf(buf, "Base: %.0f", m_curr_move.base_speed);
+                std::string base_line(1, ImGui::ColorMarkerStart);
+                if (base_label[0] != '\0') {
+                    base_line += base_label;
+                    base_line += ":";
+                } else {
+                    base_line += "Base:";
+                }
+                base_line += ImGui::ColorMarkerEnd;
+                sprintf(buf, "%s %.0f", base_line.c_str(), m_curr_move.base_speed);
                 imgui.text(buf);
+
                 // Show modifier chain
                 for (uint8_t i = 0; i < m_curr_move.speed_modifier_count; ++i) {
                     const auto& mod = m_curr_move.speed_modifiers[i];
                     const char* mod_name = get_modifier_name(mod.type);
-                    // Format based on modifier type
+
+                    // Arrow and modifier name in highlight color, speed values in regular
+                    std::string mod_label(1, ImGui::ColorMarkerStart);
+                    mod_label += "> ";
+                    mod_label += mod_name;
+                    mod_label += ImGui::ColorMarkerEnd;
+
                     if (mod.type == SpeedModifierEntry::Type::Overhang)
-                        sprintf(buf, "  -> %s (%.0f%%): %.0f", mod_name, mod.value, mod.speed_after);
+                        sprintf(buf, "%s (%.0f%%): %.0f", mod_label.c_str(), mod.value, mod.speed_after);
                     else if (mod.type == SpeedModifierEntry::Type::CurledEdge)
-                        sprintf(buf, "  -> %s (%.2f): %.0f", mod_name, mod.value, mod.speed_after);
+                        sprintf(buf, "%s (%.2f): %.0f", mod_label.c_str(), mod.value, mod.speed_after);
                     else
-                        sprintf(buf, "  -> %s (-%.0f): %.0f", mod_name, mod.value, mod.speed_after);
+                        sprintf(buf, "%s (-%.0f): %.0f", mod_label.c_str(), mod.value, mod.speed_after);
                     imgui.text(buf);
                 }
             }
